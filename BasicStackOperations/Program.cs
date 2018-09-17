f803cbf6acc2efa669212ec241c913a77edde87b
rayacho <shawnsgf@abv.bg>
2018-09-17 18:01:52 +0300

    added some programs

---
+++ BasicStackOperations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse);
+            var stack = new Stack<int>();
+            var nNumbers = Console.ReadLine().Split(' ').Select(int.Parse);
+            var existence = "false";
+            foreach (var number in input)
+            {
+                stack.Push(number);
+            }
+            int n = stack.Pop();
+            int s = stack.Pop();
+            int x = stack.Pop();
+            var stackOfNumbers = new Stack<int>();
+            var stackOfNumbers2 = new Stack<int>();
+            foreach(var chislo in nNumbers)
+            {
+                stackOfNumbers.Push(chislo);
+                stackOfNumbers2.Push(chislo);
+                if(chislo == x)
+                {
+                    existence = "true";
+                }
+            }
+            var smallestStack = new Stack<int>();
+            smallestStack.Push(stackOfNumbers.Peek());
+            for(int j = 0; j < stackOfNumbers2.Count; j++)
+            {
+                if(smallestStack.Peek() > stackOfNumbers2.Peek())
+                {
+                    smallestStack.Push(stackOfNumbers2.Pop());
+                }
+            }
+            for(int i = 0; i < s; i++)
+            {
+                stackOfNumbers.Pop();
+            }
+            if(existence == "true")
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallestStack.Pop());
+            }
+        }
+    }
+}
---
+++ MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    var startIndex = stack.Pop();
+                    string reminder = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(reminder);
+                }
+            }
+        }
+    }
+}
