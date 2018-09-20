ac310ab0cc7b000c0416290c8bdeb5f9382bdbc4
rayacho <shawnsgf@abv.bg>
2018-09-20 11:42:50 +0300

    finished stacks and queues

---
+++ RecursiveFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(input));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleTextEditor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandsCount = int.Parse(Console.ReadLine());
+            var oldVersions = new Stack<string>();
+            oldVersions.Push("");
+            var text = new StringBuilder();
+            for (int i = 0; i < commandsCount;  i++)
+            {
+                string[] commandInput = Console.ReadLine().Split();
+                int command = int.Parse(commandInput[0]);
+                switch(command)
+                {
+                    case 1:
+                        oldVersions.Push(text.ToString());
+                        text.Append(commandInput[1]);
+                        break;
+                    case 2:
+                        oldVersions.Push(text.ToString());
+                        int length = int.Parse(commandInput[1]);
+                        text.Remove(text.Length - length, length);
+                        break;
+                    case 3:
+                        int index = int.Parse(commandInput[1]);
+                        Console.WriteLine(text[index - 1]);
+                        break;
+                    case 4:
+                        text.Clear();
+                        text.Append(oldVersions.Pop());
+
+                        break;
+                }
+            }
+        }
+    }
+}
