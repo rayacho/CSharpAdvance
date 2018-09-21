1732ad8f0adaa0f58fbbf3c6c5544c9c3882c125
rayacho <shawnsgf@abv.bg>
2018-09-21 12:51:24 +0300

    added new

---
+++ MatrixOfPalindromes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MatrixOfPalindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndColumns = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int r = rowsAndColumns[0];
+            int c = rowsAndColumns[1];
+            int[,] matrix = new int[r,c];
+            for(int rows = 0; rows < r; rows++)
+            {
+                matrix[0, c] = alphabet[rows];
+                matrix[r, c] = alphabet[rows];
+                for (int cols = 0; cols < c; cols++)
+                {
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SumMatrixElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndColumns = Console.ReadLine().Split(new string[] { ", " },StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
+            int s = 0;
+            for(int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                var rowValues = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for(int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows,cols] = rowValues[cols];
+                    s += matrix[rows, cols];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(s);
+        }
+    }
+}
