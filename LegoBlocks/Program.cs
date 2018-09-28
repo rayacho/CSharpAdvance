223b1edacc0fee35fd285a2004a9cceccb48c7de
rayacho <shawnsgf@abv.bg>
2018-09-28 12:57:35 +0300

    added 5,6,7,8

---
+++ LegoBlocks/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace LegoBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] array1 = new int[n][];
+
+            int[][] array2 = new int[n][];
+
+            bool a = false;
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+
+            {
+                array1[i] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+
+            }
+        
+            for (int i = 0; i < n; i++)
+
+            {
+
+                array2[i] = Console.ReadLine()
+
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+
+                    .Select(int.Parse).Reverse().ToArray();
+
+            }
+
+            for (int i = 0; i < n - 1; i++)
+
+            {
+
+                if (array1[i].Length + array2[i].Length ==
+
+                    array1[i + 1].Length + array2[i + 1].Length)
+
+                {
+
+                    count = array1[0].Length + array2[0].Length;
+
+                    a = true;
+
+                }
+
+                else
+
+                {
+
+                    for (int y = 0; y < n; y++)
+
+                    {
+
+                        count += array1[y].Length + array2[y].Length;
+
+                    }
+
+                    Console.WriteLine("The total number of cells is: " + count);
+
+                    break;
+
+                }
+
+            }
+
+            if (a == true)
+            {
+                int[] result = new int[count];
+
+                for (int i = 0; i < n; i++)
+                {
+                    result = array1[i].Concat(array2[i]).ToArray();
+
+                    Console.WriteLine($"[{string.Join(", ", result)}]");
+
+                }
+
+            }
+        }
+    }
+}
--- RubiksMatrix/Program.cs
+++ RubiksMatrix/Program.cs
@@ -6,72 +6,218 @@ namespace RubiksMatrix
     class Program
     {
         private static int[,] matrix;
-        private static int row;
-        private static int col;
 
+        private static int rows;
+
+        private static int columns;
+        
         static void Main(string[] args)
+
         {
-            var rowsAndColumns = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+
+            int[] dimensions = Console.ReadLine()
+
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+
                 .Select(int.Parse).ToArray();
-            matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
-            int inp = 1;
-            row = matrix.GetLength(0);
-            col = matrix.GetLength(1);
 
-            for (int rows = 0; rows < row; rows++)
+            rows = dimensions[0];
+
+            columns = dimensions[1];
+
+            matrix = new int[dimensions[0], dimensions[1]];
+            
+            int number = 0;
+            
+            for (int row = 0; row < rows; row++)
             {
-                for (int cols = 0; cols < col; cols++)
-                {
-                    matrix[rows, cols] = inp;
-                    inp++;
-                }
-            }
+                for (int col = 0; col < columns; col++)
 
+                {
+                    matrix[row, col] = ++number;
+                }
+
+            }
+            
             int commandCount = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < commandCount; i++)
+
             {
                 ParseCommand();
+
             }
+            RearrangeMatrix();
         }
-        public static void ParseCommand()
+        
+        private static void RearrangeMatrix()
+
         {
-            string[] commandArgs = Console.ReadLine().Split(' ').ToArray();
+
+            int expected = 1;
+
+            for (int row = 0; row < rows; row++)
+
+            {
+
+                for (int col = 0; col < columns; col++)
+
+                {
+
+                    if (matrix[row, col] == expected)
+
+                    {
+
+                        Console.WriteLine("No swap required");
+
+                    }
+
+                    else
+
+                    {
+
+                        for (int r = 0; r < rows; r++)
+
+                        {
+
+                            for (int c = 0; c < columns; c++)
+
+                            {
+
+                                if (matrix[r, c] == expected)
+
+                                {
+
+                                    int temp = matrix[row, col];
+
+                                    matrix[row, col] = matrix[r, c];
+
+                                    matrix[r, c] = temp;
+
+                                    Console.WriteLine($"Swap({row},{col}) with({r},{c})");
+
+                                    break;
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                    expected++;
+
+                }
+
+            }
+
+        }
+
+        private static void ParseCommand()
+
+        {
+
+            string[] commandArgs = Console.ReadLine().Split();
+
             string command = commandArgs[1];
+
             int index = int.Parse(commandArgs[0]);
+
             int rotations = int.Parse(commandArgs[2]);
 
-            switch(command)
+            switch (command)
+
             {
+
                 case "up":
-                    MoveColumn(index, row - rotations);
+
+                    MoveColumn(index, rows - (rotations % rows));
+
                     break;
+
                 case "down":
-                    MoveColumn(index, rotations);
+
+                    MoveColumn(index, (rotations % rows));
+
                     break;
+
                 case "left":
-                    MoveRow(index, col - rotations);
+
+                    MoveRow(index, columns - (rotations % columns));
+
                     break;
+
                 case "right":
-                    MoveRow(index, rotations);
+
+                    MoveRow(index, (rotations % columns));
+
                     break;
+
             }
+
         }
-        private static void MoveColumn(int index, int rotations)
+    private static void MoveRow(int index, int rotations)
+
         {
-            rotations %= col;
 
-            int[] tempArray = new int[col];
+            //rotations %= columns;
 
-            for (int c = 0; c < col; c++)
+            int[] tempArray = new int[columns];
+
+            for (int col = 0; col < columns; col++)
+
             {
-                int replacementIndex = c + rotations;
-                replacementIndex %= col;
-                tempArray[replacementIndex] = matrix[index, c];
+
+                int replacementIndex = col + rotations;
+                replacementIndex %= columns;
+                tempArray[replacementIndex] = matrix[index, col];
+
             }
+
+            for (int col = 0; col < columns; col++)
+
+            {
+                matrix[index, col] = tempArray[col];
+            }
+
         }
-        private static void MoveRow(int index, int rotation)
+        
+        private static void MoveRow()
+
         {
+
+
+
+        }
+        
+        private static void MoveColumn(int index, int rotations)
+
+        {
+            
+            // rotations %= rows;
+
+            int[] tempArray = new int[rows];
+
+            for (int row = 0; row < columns; row++)
+
+            {
+            
+                int replacementIndex = row + rotations;
+                
+                replacementIndex %= rows;
+
+                tempArray[replacementIndex] = matrix[index, row];
+
+            }
+
+            for (int row = 0; row < rows; row++)
+
+            {
+
+                matrix[row, index] = tempArray[row];
+            }
+
         }
     }
 }
