e32d9a4f5398a3b6c2354bdad86d0b3d5a6d8f6e
rayacho <shawnsgf@abv.bg>
2018-10-03 11:53:59 +0300

    finished multidimensional arrays

---
+++ Ex1/Ex1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Ex1
+{
+    class Ex1
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("Ex1.cs"))
+            {
+                var lineNumber = 1;
+
+                string line;
+                while((line = Console.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Line {lineNumber}: " + line);
+                    lineNumber++;
+                }
+            }
+            
+        }
+    }
+}
