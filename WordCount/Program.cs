c1548eb7ff24822abe2dd23069a3a047aa77b27f
rayacho <shawnsgf@abv.bg>
2018-10-05 13:00:13 +0300

    wordcount

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -9,14 +9,30 @@ namespace WordCount
         {
             using (var readStream = new StreamReader(@"D:\text.txt"))
             {
-                using (var writeStream = new StreamWriter("linenumbers.txt"))
+                using (var readWords = new StreamReader(@"C:\Users\RAYA CHORBADZHIYSKA\Desktop\words.txt"))
                 {
-                    int lineNumber = 1;
-                    string line;
-                    while ((line = readStream.ReadLine()) != null)
+                    using (var writeStream = new StreamWriter(@"C:\Users\RAYA CHORBADZHIYSKA\Desktop\CSharpAdvanced\CSharpAdvance\WordCount\result.txt"))
                     {
-                        writeStream.WriteLine($"Line {lineNumber}: " + line);
-                        lineNumber++;
+                        int times1 = 0, times2 = 0, times3 = 0;
+                        string firstWord = readWords.ReadLine();
+                        string secondWord = readWords.ReadLine();
+                        string thirdWord = readWords.ReadLine();
+                        string line = readStream.ReadLine();
+                        while (line != null)
+                        {
+                            string[] input = line.Split();
+                            int length = input.Length;
+                            for(int i = 0; i < length; i++)
+                            {
+                                if (firstWord == input[i]) times1++;
+                                if (secondWord == input[i]) times2++;
+                                if (thirdWord == input[i]) times3++;
+                            }
+                            line = readStream.ReadLine();
+                        }
+                        writeStream.WriteLine(firstWord + ' ' + times1);
+                        writeStream.WriteLine(secondWord + ' ' + times2);
+                        writeStream.WriteLine(thirdWord + ' ' + times3);
                     }
                 }
             }
