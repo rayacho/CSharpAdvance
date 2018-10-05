ee0b5e3bcbd8463b04ef635d6aaf1aba4c7486a7
rayacho <shawnsgf@abv.bg>
2018-10-05 11:04:47 +0300

    added new streams

---
+++ WordCount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var readStream = new StreamReader(@"D:\text.txt"))
+            {
+                using (var writeStream = new StreamWriter("linenumbers.txt"))
+                {
+                    int lineNumber = 1;
+                    string line;
+                    while ((line = readStream.ReadLine()) != null)
+                    {
+                        writeStream.WriteLine($"Line {lineNumber}: " + line);
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
