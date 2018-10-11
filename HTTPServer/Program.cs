2749523bed28881a46e3387a772798b72fea95b6
rayacho <shawnsgf@abv.bg>
2018-10-11 11:52:28 +0300

    added 5,6,8,9

---
+++ HTTPServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		IPAddress address = IPAddress.Parse("127.0.0.1");
+		int port = 8081;
+
+		TcpListener tcpListener = new TcpListener(address, port);
+		tcpListener.Start();
+
+		Console.WriteLine($"Listening to TCP clients at 127.0.0.1:{port}");
+
+		Task.Run(async () => await Connect(tcpListener));
+
+		Console.ReadLine();
+	}
+
+	public static async Task Connect(TcpListener tcpListener)
+	{
+		Console.WriteLine("Waiting for client...");
+
+		while (true)
+		{
+			TcpClient client = await tcpListener.AcceptTcpClientAsync();
+
+			Console.WriteLine("Client connected.\n");
+
+			byte[] buffer = new byte[1024];
+
+			await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+
+			string message = Encoding.ASCII.GetString(buffer);
+
+			Console.WriteLine(message);
+
+			byte[] response = Encoding.ASCII.GetBytes("<body><h1>Hello from server!</h1></body>");
+
+			await Task.Delay(5000);
+
+			await client.GetStream().WriteAsync(response, 0, response.Length);
+
+			Console.WriteLine("Closing connection");
+
+			client.GetStream().Dispose();
+		}
+	}
+}
--- RadioactiveBunnies/Program.cs
+++ RadioactiveBunnies/Program.cs
@@ -3,340 +3,196 @@ using System.Linq;
 
 namespace RadioactiveBunnies
 {
-    class Program
-    {
-        
-            static char[,] board;
-
-            static int playerRow;
-
-            static int playerCol;
-
-            static int rows;
-
-            static int columns;
-
-
-
-
-
-            static void Main(string[] args)
-
-            {
-
-                int[] dimensions = Console.ReadLine()
-
-                    .Split().
-
-                    Select(int.Parse).
-
-                    ToArray();
-
-
-
-                board = ReadAndFillMatrix(dimensions);
-
-
-
-                char[] movements = Console.ReadLine().ToCharArray();
-
-
-
-                foreach (char move in movements)
-
-                {
-
-                    int[] previouslocation = MovePlayer(move);
-
-                    MultiplyBunnies();
-
-
-
-                    if (IsPlayerOnBoard())
-
-                    {
-
-                        if (board[playerRow, playerCol] == 'B')
-
-                        {
-
-                            Die();
-
-                        }
-
-                        continue;
-
-                    }
-
-
-
-                    Win(previouslocation);
-
-                }
-
-            }
-
-
-            private static void Die()
-
-            {
-
-                PrintBoard();
-
-
-
-                Console.WriteLine($"dead: {playerRow} {playerCol}");
-
-                Environment.Exit(0);
-
-            }
-
-            private static void Win(int[] previouslocation)
-
-            {
-
-                PrintBoard();
-
-
-
-                int row = previouslocation[0];
-
-                int col = previouslocation[1];
-
-
-
-                Console.WriteLine($"won: {row} {col}");
-
-                Environment.Exit(0);
-
-            }
-            
-            private static bool IsPlayerOnBoard()
-
-            {
-
-                bool validRow = playerRow >= 0 && playerRow < rows;
-
-                bool validCol = playerCol >= 0 && playerCol < columns;
-
-                if (validRow && validCol)
-
-                {
-
-                    return true;
-
-                }
-
-                return false;
-
-            }
-            
-            private static void PrintBoard()
-
-            {
-
-                for (int row = 0; row < rows; row++)
-
-                {
-
-                    for (int col = 0; col < columns; col++)
-
-                    {
-
-                        Console.Write(board[row, col]);
-
-                    }
-
-                    Console.WriteLine();
-
-                }
-
-            }
-            
-            private static void MultiplyBunnies()
-
-            {
-
-                for (int row = 0; row < rows; row++)
-
-                {
-
-                    for (int col = 0; col < columns; col++)
-
-                    {
-
-                        if (board[row, col] == 'B')
-
-                        {
-
-                            if (row > 0)
-
-                            {
-
-                                NewBunny(row - 1, col);
-
-                            }
-
-                            if (row < rows - 1)
-
-                            {
-
-                                NewBunny(row + 1, col);
-
-                            }
-
-                            if (col > 0)
-
-                            {
-
-                                NewBunny(row, col - 1);
-
-                            }
-
-                            if (col < columns - 1)
-
-                            {
-
-                                NewBunny(row, col + 1);
-
-                            }
-
-                        }
-
-                    }
-
-                }
-                
-                for (int row = 0; row < rows; row++)
-
-                {
-
-                    for (int col = 0; col < columns; col++)
-
-                    {
-
-                        if (board[row, col] == 'X')
-
-                        {
-
-                            board[row, col] = 'B';
-
-                        }
-
-                    }
-
-                }
-
-            }
-            
-            private static void NewBunny(int row, int col)
-
-            {
-
-                if (board[row, col] != 'B')
-
-                {
-
-                    board[row, col] = 'X';
-
-                }
-
-            }
-            
-            private static int[] MovePlayer(char move)
-
-            {
-
-                int[] previousLocation = { playerRow, playerCol };
-
-
-
-                switch (move)
-
-                {
-
-                    case 'U':
-                        playerRow--;
-
-                        break;
-
-                    case 'D':
-
-                        playerRow++;
-
-                        break;
-
-                    case 'L':
-
-                        playerCol--;
-
-                        break;
-
-                    case 'R':
-
-                        playerCol++;
-
-                        break;
-
-                }
-
-                if (IsPlayerOnBoard() && board[playerRow, playerCol] != 'B')
-
-                {
-
-                    board[playerRow, playerCol] = 'P';
-
-                }
-
-                int oldRow = previousLocation[0];
-
-                int oldCol = previousLocation[1];
-
-                board[oldRow, oldCol] = '.';
-
-                return previousLocation;
-
-            }
-            
-            private static char[,] ReadAndFillMatrix(int[] dimensions)
-
-            {
-
-                rows = dimensions[0];
-
-                columns = dimensions[1];
-
-                char[,] matrix = new char[rows, columns];
-
-                for (int row = 0; row < rows; row++)
-
-                {
-
-                    char[] rowInput = Console.ReadLine().ToCharArray();
-
-                    for (int col = 0; col < columns; col++)
-
-                    {
-
-                        matrix[row, col] = rowInput[col];
-
-                        if (rowInput[col] == 'P')
-
-                        {
-
-                            playerRow = row;
-
-                            playerCol = col;
-
-                        }
-
-                    }
-
-                }
-                
-                return matrix;
-
-            }
-        }
-    }
+	class Program
+	{
+		static char[,] board;
+		static int playerRow;
+		static int playerCol;
+		static int rows;
+		static int columns;
+
+		static void Main(string[] args)
+		{
+			int[] dimensions = Console.ReadLine()
+				.Split().
+				Select(int.Parse).
+				ToArray();
+
+			board = ReadAndFillMatrix(dimensions);
+			char[] movements = Console.ReadLine().ToCharArray();
+
+			foreach (char move in movements)
+			{
+				int[] previouslocation = MovePlayer(move);
+				MultiplyBunnies();
+
+				if (IsPlayerOnBoard())
+				{
+					if (board[playerRow, playerCol] == 'B')
+					{
+						Die();
+					}
+
+					continue;
+				}
+
+				Win(previouslocation);
+			}
+		}
+
+		private static void Die()
+		{
+			PrintBoard();
+			Console.WriteLine($"dead: {playerRow} {playerCol}");
+			Environment.Exit(0);
+		}
+
+		private static void Win(int[] previouslocation)
+		{
+			PrintBoard();
+			int row = previouslocation[0];
+			int col = previouslocation[1];
+			Console.WriteLine($"won: {row} {col}");
+			Environment.Exit(0);
+		}
+
+		private static bool IsPlayerOnBoard()
+		{
+			bool validRow = playerRow >= 0 && playerRow < rows;
+			bool validCol = playerCol >= 0 && playerCol < columns;
+
+			if (validRow && validCol)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		private static void PrintBoard()
+		{
+			for (int row = 0; row < rows; row++)
+			{
+				for (int col = 0; col < columns; col++)
+				{
+					Console.Write(board[row, col]);
+				}
+
+				Console.WriteLine();
+			}
+		}
+
+		private static void MultiplyBunnies()
+
+		{
+			for (int row = 0; row < rows; row++)
+			{
+				for (int col = 0; col < columns; col++)
+				{
+					if (board[row, col] == 'B')
+					{
+						if (row > 0)
+						{
+							NewBunny(row - 1, col);
+						}
+
+						if (row < rows - 1)
+						{
+							NewBunny(row + 1, col);
+						}
+
+						if (col > 0)
+						{
+							NewBunny(row, col - 1);
+						}
+
+						if (col < columns - 1)
+						{
+							NewBunny(row, col + 1);
+						}
+					}
+				}
+			}
+
+			for (int row = 0; row < rows; row++)
+			{
+				for (int col = 0; col < columns; col++)
+				{
+					if (board[row, col] == 'X')
+					{
+						board[row, col] = 'B';
+					}
+				}
+			}
+		}
+
+		private static void NewBunny(int row, int col)
+		{
+			if (board[row, col] != 'B')
+			{
+				board[row, col] = 'X';
+			}
+		}
+
+		private static int[] MovePlayer(char move)
+		{
+			int[] previousLocation = { playerRow, playerCol };
+
+			switch (move)
+			{
+				case 'U':
+					playerRow--;
+					break;
+
+				case 'D':
+					playerRow++;
+					break;
+
+				case 'L':
+					playerCol--;
+					break;
+
+				case 'R':
+					playerCol++;
+					break;
+			}
+
+			if (IsPlayerOnBoard() && board[playerRow, playerCol] != 'B')
+			{
+				board[playerRow, playerCol] = 'P';
+			}
+
+			int oldRow = previousLocation[0];
+			int oldCol = previousLocation[1];
+			board[oldRow, oldCol] = '.';
+
+			return previousLocation;
+		}
+
+		private static char[,] ReadAndFillMatrix(int[] dimensions)
+		{
+			rows = dimensions[0];
+			columns = dimensions[1];
+			char[,] matrix = new char[rows, columns];
+
+			for (int row = 0; row < rows; row++)
+			{
+				char[] rowInput = Console.ReadLine().ToCharArray();
+
+				for (int col = 0; col < columns; col++)
+				{
+					matrix[row, col] = rowInput[col];
+
+					if (rowInput[col] == 'P')
+					{
+						playerRow = row;
+						playerCol = col;
+					}
+				}
+			}
+
+			return matrix;
+		}
+	}
+}
 
